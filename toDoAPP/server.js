1e00cbab83e2c6f1b625ec1e6e8b4ba065567eb0
Duck Hyung Jun <ddokkangjun@gmail.com>
2023-05-09 13:20:08 +0900

    session auth setting

--- toDoAPP/server.js
+++ toDoAPP/server.js
@@ -6,6 +6,14 @@ app.use('/public', express.static('public'));
 const methodOverride = require('method-override');
 app.use(methodOverride('_method'));
 
+// session auth setting
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
+const session = require('express-session');
+app.use(session({ secret: '비밀코드', resave: true, saveUninitialized: false }));
+app.use(passport.initialize());
+app.use(passport.session());
+
 var db;
 const MongoClient = require('mongodb').MongoClient;
 MongoClient.connect(
@@ -108,3 +116,28 @@ app.get('/detail/:id', function (request, response) {
   })
 })
 
+// sesson-based login request & Authenticate
+app.get('/login', function (request, response) {
+  response.render('login.ejs');
+})
+app.post('/login', passport.authenticate('local', { failureRedirect: '/fail' }), function (요청, 응답) {
+  응답.redirect('/')
+});
+passport.use(new LocalStrategy({
+  usernameField: 'id',
+  passwordField: 'pw',
+  session: true,
+  passReqToCallback: false,
+}, function (입력한아이디, 입력한비번, done) {
+  //console.log(입력한아이디, 입력한비번);
+  db.collection('login').findOne({ id: 입력한아이디 }, function (에러, 결과) {
+    if (에러) return done(에러)
+
+    if (!결과) return done(null, false, { message: '존재하지않는 아이디요' })
+    if (입력한비번 == 결과.pw) {
+      return done(null, 결과)
+    } else {
+      return done(null, false, { message: '비번틀렸어요' })
+    }
+  })
+}));
